ee8c557f7bf095f442dc69848ab20db6032d9aeb
Nvch0 <xignacio2002x@gmail.com>
2023-10-28 16:07:00 -0300

    Boton de retroceso terminado

--- myapp/src/app/pages/main/home/home.page.ts
+++ myapp/src/app/pages/main/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
+import { AddUpdateProductComponent } from 'src/app/shared/components/add-update-product/add-update-product.component';
 
 @Component({
   selector: 'app-home',
@@ -20,5 +21,14 @@ export class HomePage implements OnInit {
   signOut(){
     this.firebaseSvc.signOut();
   }
+
+  //=============Agregar Clases==============
+  addUpdateProduct(){
+    this.utilsSvc.presentModal({
+      component: AddUpdateProductComponent,
+      cssClass:'add-update-modal'
+    })
+  }
+
 
 }
--- myapp/src/app/services/utils.service.ts
+++ myapp/src/app/services/utils.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
+import { LoadingController, ModalController, ModalOptions, ToastController, ToastOptions } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,7 @@ export class UtilsService {
 
   loadingCtrl = inject(LoadingController);
   toastCtrl = inject(ToastController);
+  modalCtrl = inject(ModalController)
   router = inject(Router)
 
 
@@ -40,6 +41,19 @@ export class UtilsService {
 
   getFromLocalStorage(key: string){
     return JSON.parse (localStorage.getItem(key))
+  }
+
+  //============Modal 1==========
+  async presentModal(opts: ModalOptions) {
+    const modal = await this.modalCtrl.create(opts);
+    await modal.present();
+
+    const {data} = await modal.onWillDismiss();
+    if(data) return data;
+  
+  }
+  dismissModal(data?: any) {
+    return this.modalCtrl.dismiss(data);
   }
 
 
